37a280eeab678252ed699868a16e283aa735ff18
pcaric <pcaric@algebra.hr>
2025-05-20 14:55:43 +0200

    dashboard added, picture and update changed

--- Travel.Web/Controllers/HomeController.cs
+++ Travel.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Mvc;
 using Travel.Web.Models;
 
@@ -28,5 +29,26 @@ namespace Travel.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public IActionResult Dashboard()
+        {
+
+            //token is used to see if the user logged in is admin or not
+            var token = HttpContext.Session.GetString("JWToken");
+            if (string.IsNullOrEmpty(token)) return RedirectToAction("Login", "Account");
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+            var isAdmin = jwtToken.Claims.FirstOrDefault(c => c.Type == "isAdmin")?.Value;
+
+            ViewBag.IsAdmin = isAdmin == "True" || isAdmin == "true" || isAdmin == "1";
+            // TempData is used to transffer data to the nest redirection place
+            TempData["JwtToken"] = token;
+            return View();
+        }
+
+
+
     }
 }
